7d88aa5d8607cce633f4f8d78b9f5e34cfef9701
Mykhailo Odrekhivskyi <skipokylas@gmail.com>
2019-04-14 17:44:40 +0300

    Add css/scss loaders

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import sum from './app/sum';
+import './assets/css/main.css'
+import './assets/scss/main.scss'
+
 
 console.info(sum(2,3));
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,52 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // address to package in package.json
 
 module.exports = {
     entry: {
         app: './src/index.js'
     },
     output: {
-        filename: '[name].js',  // '[name].js' for more then one entry point
+        filename: '[name].js',
         path: path.resolve(__dirname,'./dist'),
         publicPath:'/dist' // for dev-server
     },
     module: {
-        rules: [{
-            test: /\.js$/,
-            loader: 'babel-loader',
-            exclude: '/node_modules/'
-        }]
+        rules: [  // loaders for processing js, css... files.
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader, // the loader helps separate css and js
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                   {
+                       loader: 'css-loader',
+                       options: { sourceMap: true }
+                   },
+                   {
+                    loader: 'sass-loader',
+                    options: { sourceMap: true }
+                   }
+                ]
+            }
+        ]
     },
     devServer: {
         overlay: true // show error
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css' 
+        }),
+    ],
 }
